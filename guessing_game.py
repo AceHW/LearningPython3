c0571e56e060580590f899c3495539a36cb00e3b
Ace <36691877+AceHW@users.noreply.github.com>
2020-04-12 17:10:08 -0500

    added guessing_game.py
    mostly copied, some by hand
    
    https://www.devdungeon.com/content/python-tutorial-guessing-game 
    https://www.youtube.com/watch?v=duwiPfyRfnw

---
+++ guessing_game.py
@@ -0,0 +1,35 @@
+import random
+number_of_guesses=4
+user_won = False
+
+print("Welcome to the guessing game!")
+
+
+# Computer guesses a random number between 1 and 10
+correct_answer = random.randint(1, 10)
+
+while number_of_guesses > 0:
+# User guesses the number
+    user_guess = input("Guess my number: ")
+    user_guess = int(user_guess)
+    if user_guess == correct_answer:
+        print("Good guess")
+        print("You're correct")
+        user_won=True
+        break
+
+    # Computer tells user whether guess was too high or too low
+    elif user_guess > correct_answer:
+        print("Sorry, guessed too high")
+    elif user_guess < correct_answer:
+        print("Sorry, guessed too low")
+
+
+    # User has 3 guesses before losing the game
+    number_of_guesses -= 1
+
+
+if user_won ==True:
+    print("You won!")
+else:
+    print("You lost ")
