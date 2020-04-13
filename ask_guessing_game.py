7bc9ab37af09ca3d550fb3672a6506fd2b40affe
Ace <36691877+AceHW@users.noreply.github.com>
2020-04-13 18:07:24 -0500

    renamed vars a and b to the more self-explanatory minrand and maxrand

--- ask_guessing_game.py
+++ ask_guessing_game.py
@@ -1,23 +1,23 @@
 import random
 number_of_guesses = 4
 user_won = False
-a = 1
-b = 10
+minrand = 1
+maxrand = 10
 print("Welcome to the (ask) guessing game!")
 
 print("This version asks what numbers you want the random number to be between and how many guesses you want to have.")
 
-a = input("What number do you want the random pick to start at? Default and recommended value is 1 : ")
-a = int(a)
+minrand = input("What number do you want the random pick to start at? Default and recommended value is 1 : ")
+minrand = int(minrand)
 
-b = input("What number do you want the random pick to max out at? Default value is 10 : ")
-b = int(b)
+maxrand = input("What number do you want the random pick to max out at? Default value is 10 : ")
+maxrand = int(maxrand)
 
 number_of_guesses = input("How many guesses do you want? : ")
 number_of_guesses = int(number_of_guesses)
 
 # Computer guesses a random number between 1 and 10
-correct_answer = random.randint(a, b)
+correct_answer = random.randint(minrand, maxrand)
 
 while number_of_guesses > 0:
 # User guesses the number
