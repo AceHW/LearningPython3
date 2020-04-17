b95374a6d15036ad27fe95ef41533bb45542e45d
Ace <36691877+AceHW@users.noreply.github.com>
2020-04-17 13:35:08 -0500

    Add files via upload
    fixed some syntax errors

--- solve_for_missing_in_equivalent_fractions.py
+++ solve_for_missing_in_equivalent_fractions.py
@@ -39,19 +39,19 @@ if missing=="b":
     h=((a*d)/c)
     if (e==g and e==h and g==h and ((a/e)==(c/d)))==True:
         print("\n"+"the methods seem to agree that b is "+str(e)+" and (a/b)=(c/d)")
-    elif (e==g and e==h and g==h and ((a/e==(c/d)))==False:
+    elif (e==g and e==h and g==h and ((a/e==(c/d))))==False:
         print("hmm. The methods don't agree and/or the check that (a/b)=(c/d) has failed when the result from method 1 is set as b. This shouldn't happen. Please report this problem and what was inputed to cause this.")
         print("method 1 of 3 says b is "+str(e))
         print("method 2 of 3 says b is "+str(g))
         print("method 3 of 3 says b is "+str(h))
         if ((a/e)==(c/d))==True:
-			print("((a/b)==(c/d)) is True")
-		elif ((a/e)==(c/d))==False:
-			print("((a/b)=(c/d)) is False when "+str(e)" from method 1 is set as b")
+            print("((a/b)==(c/d)) is True")
+        elif ((a/e)==(c/d))==False:
+            print("((a/b)=(c/d)) is False when "+str(e)+" from method 1 is set as b")
 
 if missing=="c":
-	a=float(input("enter a: “))
-	b=float(input("enter b: “))
+    a=float(input("enter a: "))
+    b=float(input("enter b: "))
     print("c is being solved for")
     d=float(input("enter d: "))
     print()
@@ -59,35 +59,34 @@ if missing=="c":
     f=((d/b)*a)
     h=((a*d)/b)
     if (e==f and e==h and f==h and ((a/b)==(e/d)))==True:
-		print("\n"+"the methods seem to agree that c is "+str(e)+" and (a/b)=(c/d)")
-    elif (e==f and e==h and f==h and ((a/b==(e/d)))==False:
-		print("hmm. The methods don't agree and/or the check that (a/b)=(c/d) has failed when the result from method 1 is set as c. This shouldn't happen. Please report this problem and what was inputed to cause this.")
-		print("method 1 of 3 says c is "+str(e))
-		print("method 1 of 3 says c is "+str(f))
-		print("method 1 of 3 says c is "+str(h))
-		if ((a/b)==(e/d))==True:
-			print("((a/b)==(c/d)) is True")
-		elif ((a/b)==(e/d))==False:
-			print("((a/b)=(c/d)) is False when "+str(e)" from method 1 is set as c")
+        print("\n"+"the methods seem to agree that c is "+str(e)+" and (a/b)=(c/d)")
+    elif (e==f and e==h and f==h and ((a/b==(e/d))))==False:
+        print("hmm. The methods don't agree and/or the check that (a/b)=(c/d) has failed when the result from method 1 is set as c. This shouldn't happen. Please report this problem and what was inputed to cause this.")
+        print("method 1 of 3 says c is "+str(e))
+        print("method 1 of 3 says c is "+str(f))
+        print("method 1 of 3 says c is "+str(h))
+        if ((a/b)==(e/d))==True:
+            print("((a/b)==(c/d)) is True")
+        elif ((a/b)==(e/d))==False:
+            print("((a/b)=(c/d)) is False when "+str(e)+" from method 1 is set as c")
 
 if missing=="d":
-	a=float(input("enter a: “))
-	b=float(input("enter b: “))
-	c=float(input("enter c: "))
+    a=float(input("enter a: "))
+    b=float(input("enter b: "))
+    c=float(input("enter c: "))
     print("d is being solved for")
     print()
     e=((b/a)*c)
     f=((c/a)*b)
     g=((c*b)/a)
     if (e==f and e==g and f==g and ((a/b)==(c/e)))==True:
-		print("\n"+"the methods seem to agree that d is "+str(e)+" and (a/b)=(c/d)")
-	elif (e==f and e==g and f==g and ((a/b)==(c/e)))==False:
-		print("hmm. The methods don't agree and/or the check that (a/b)=(c/d) has failed when the result from method 1 is set as d. This shouldn't happen. Please report this problem and what was inputed to cause this.")
-		print("method 1 of 3 says d is "+str(e))
-		print("method 1 of 3 says d is "+str(f))
-		print("method 1 of 3 says d is "+str(g))
-		if ((a/b)==(c/e))==True:
-			print("((a/b)==(c/d)) is True")
-		elif ((a/b)==(c/e))==False:
-			print("((a/b)=(c/d)) is False when "+str(e)" from method 1 is set as d")
-
+        print("\n"+"the methods seem to agree that d is "+str(e)+" and (a/b)=(c/d)")
+    elif (e==f and e==g and f==g and ((a/b)==(c/e)))==False:
+        print("hmm. The methods don't agree and/or the check that (a/b)=(c/d) has failed when the result from method 1 is set as d. This shouldn't happen. Please report this problem and what was inputed to cause this.")
+        print("method 1 of 3 says d is "+str(e))
+        print("method 1 of 3 says d is "+str(f))
+        print("method 1 of 3 says d is "+str(g))
+        if ((a/b)==(c/e))==True:
+            print("((a/b)==(c/d)) is True")
+        elif ((a/b)==(c/e))==False:
+            print("((a/b)=(c/d)) is False when "+str(e)+" from method 1 is set as d")
